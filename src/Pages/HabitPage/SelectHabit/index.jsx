e18dc88ec0818d8ef3a68d899080689e98ea49f7
guilh <guilherme00rogerio@gmail.com>
2023-01-10 11:38:55 -0300

    Adicionando os estilos ao SelectHabit

--- src/Pages/HabitPage/SelectHabit/index.jsx
+++ src/Pages/HabitPage/SelectHabit/index.jsx
@@ -54,6 +54,28 @@ export default SelectHabit
 
 const styles = StyleSheet.create({
   boxStyle: {
-    
-  }
-})
+    borderWidth: 1,
+    borderColor: "white",
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  inputStyle: {
+    color: "white",
+  },
+  dropdownStyle: {
+    borderWidth: 0,
+  },
+  dropdownItemStyle: {
+    borderWidth: 1,
+    borderColor: "#BBBB",
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+  dropdownTextStyle: {
+    color: "#BBBBBB",
+  },
+  arrow: {
+    width: 20,
+    height: 20,
+  },
+});
